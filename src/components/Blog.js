a015bc62b586a4ddec389e3857b76521e3426477
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-07-10 17:27:05 -0700

    exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,15 @@
+import Togglable from "./Togglable"
+
 const Blog = ({blog}) => (
   <div>
-    {blog.title} {blog.author}
+    {blog.title} {blog.author} 
+    <Togglable buttonLabel='View'>
+      <ul>
+        <li>{blog.url}</li>
+        <li>Likes: {blog.likes}</li>
+        <li>{blog.user[0].name}</li>
+      </ul>
+    </Togglable>
   </div>  
 )
 
