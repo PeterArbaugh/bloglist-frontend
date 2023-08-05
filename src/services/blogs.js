c65feca609ca0908ea54f085155b5ce037fdd1c3
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-08-05 09:50:50 -0700

    lint fixes

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,53 +2,52 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 const postSingle = ( blog, token ) => {
 
-  const config = {
-    headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `bearer ${token}`
-  }
-}
-  
-  const request = axios.post(baseUrl, blog, config)
-  console.log('blog post request', baseUrl, blog, config)
-  return request.then(response => response.data)
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `bearer ${token}`
+        }
+    }
+
+    const request = axios.post(baseUrl, blog, config)
+    console.log('blog post request', baseUrl, blog, config)
+    return request.then(response => response.data)
 }
 
 const likeSingle = ( id, blog, token ) => {
-  const config = {
-    headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `bearer ${token}`
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `bearer ${token}`
+        }
     }
-  }
 
-  const blogUrl = baseUrl + '/' + id
+    const blogUrl = baseUrl + '/' + id
 
-  const request = axios.put(blogUrl, blog, config)
-  console.log('blog put request')
-  return request.then(response => response.data)
+    const request = axios.put(blogUrl, blog, config)
+    console.log('blog put request')
+    return request.then(response => response.data)
 }
 
 const deleteSingle = (id, token) => {
-  const config = {
-    headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `bearer ${token}`
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `bearer ${token}`
+        }
     }
-  }
 
-  const blogUrl = baseUrl + '/' + id
+    const blogUrl = baseUrl + '/' + id
 
-  const request = axios.delete(blogUrl, config)
-  console.log('delete request', request)
-  return request.then(response => response.data)
+    const request = axios.delete(blogUrl, config)
+    console.log('delete request', request)
+    return request.then(response => response.data)
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, postSingle, likeSingle, deleteSingle }
