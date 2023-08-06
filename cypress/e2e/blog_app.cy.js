347ab88c48ae91b8c4a60b7998db85e997d1cb72
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-08-05 17:30:02 -0700

    exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', () => {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+            name: 'Cypress Tester',
+            username: 'ctester',
+            password: 'test_password'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -8,6 +14,28 @@ describe('Blog app', () => {
         cy.contains('Blog app')
         cy.contains('Login to application')
     })
+
+    describe('Login', function() {
+        it('suceeds with correct credentials', function() {
+            cy.contains('Login').click()
+            cy.get('#username').type('ctester')
+            cy.get('#password').type('test_password')
+            cy.get('#login-button').click()
+
+            cy.contains('Cypress Tester is logged in.')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.contains('Login').click()
+            cy.get('#username').type('ctester')
+            cy.get('#password').type('wrong password')
+            cy.get('#login-button').click()
+
+            cy.contains('Wrong credentials')
+        })
+    })
+
+
 
 
 })
